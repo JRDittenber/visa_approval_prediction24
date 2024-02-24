d7151448c42c8ff54f9cf23c151bf9449333b91c
JRDittenber <jeff.dittenber@hotmail.com>
2024-02-23 18:53:50 -0600

    Set up and Requirments

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    
+    name='Visa_Approval_Prediction',
+    version= '0.0.0',
+    author = 'J.Dittenber',
+    author_email= 'jeff.dittenber@hotmail.com',
+    packages = find_packages(),
+    
+)
